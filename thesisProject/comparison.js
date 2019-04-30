59755208bcd803face7d66aadcebb683b34f51da
aaditirokade <aaditi@newschool.edu>
2019-04-30 12:34:21 -0400

    it9

--- thesisProject/comparison.js
+++ thesisProject/comparison.js
@@ -6,6 +6,8 @@ var colors, tabs, id;
 
 // -------------- Load JSON  -------------------
 d3.json('./data/thesis_data.json').then((data) => {
+    
+    console.log(data);
   
 
     //-------------- SVG for DV -------------------
@@ -15,9 +17,7 @@ d3.json('./data/thesis_data.json').then((data) => {
 
     //-------------- DIV for TOOLTIP -------------------
     var div = d3.select("body").append("div").attr("id", "tooltip").style('visibility','hidden');
-
-
-
+    
     
     // -------------- Set Margins Width & Height for DV ------------------
     var col10 = document.getElementById("this");
@@ -52,17 +52,20 @@ d3.json('./data/thesis_data.json').then((data) => {
      // --------------------- Plot circles -----------------
      var marg = height/10;
      
-     
-      // AIC
-      var rounds_aic = svg_plot.selectAll(".bar_aic").data(data).enter().append("circle").attr("class", "bar_aic")
+     function draw(selection){
+         
+         svg_plot.selectAll('circle').remove();
+         rounds = [];
+         
+          var rounds_aic = svg_plot.selectAll(".bar_aic").data(data).enter().append("circle").attr("class", "bar_aic")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg; })
-                     .attr('r', function(d) { return d.AIC_can*6; }).attr('fill', '#d75b68')
+                     .attr('r', function(d) { return d["AIC_"+selection]*6; }).attr('fill', '#d75b68')
                      .attr('opacity', 0.8).on("mouseover", function(d) {
     
                             // TOOLTIP : Visibility, transition, opacity, content
                             document.getElementById("tooltip").style.visibility='visible';
                             div.transition().duration(200).style("opacity", 1);
-                            div.html('AIC : '+ d.month +' '+ d.year +': '+ d.AIC_can +'%');
+                            div.html('AIC : '+ d.month +' '+ d.year +': '+ d["AIC_"+selection] +'%');
                       })
                       .on("mouseout", function(d) {
                             div.transition().duration(500).attr('fill','black').style("opacity", 0);
@@ -74,7 +77,7 @@ d3.json('./data/thesis_data.json').then((data) => {
      // JAI
       var rounds_jai = svg_plot.selectAll(".bar_jai").data(data).enter().append("circle").attr("class", "bar_jai")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg*2; })
-                     .attr('r', function(d) { return d.JAI_can*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
+                     .attr('r', function(d) { return d["JAI_"+selection]*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
                      .attr('opacity', 0.8).on("mouseover", function(d) {
 
                          // TOOLTIP : Visibility, transition, opacity, content
@@ -92,7 +95,7 @@ d3.json('./data/thesis_data.json').then((data) => {
     // SEG
       var rounds_seg = svg_plot.selectAll(".bar_seg").data(data).enter().append("circle").attr("class", "bar_seg")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg*3; })
-                     .attr('r', function(d) { return d.SEG_can*5; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
+                     .attr('r', function(d) { return d["SEG_"+selection]*5; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
                      .attr('opacity', 0.8).on("mouseover", function(d) {
 
                             // TOOLTIP : Visibility, transition, opacity, content
@@ -110,7 +113,7 @@ d3.json('./data/thesis_data.json').then((data) => {
     // GOW
       var rounds_gow = svg_plot.selectAll(".bar_gow").data(data).enter().append("circle").attr("class", "bar_gow")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg*4; })
-                     .attr('r', function(d) { return d.GOW_can*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
+                     .attr('r', function(d) { return d["GOW_"+selection]*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
                      .attr('opacity', 0.8).on("mouseover", function(d) {
 
                             // TOOLTIP : Visibility, transition, opacity, content
@@ -128,7 +131,7 @@ d3.json('./data/thesis_data.json').then((data) => {
     // IGO
       var rounds_igo = svg_plot.selectAll(".bar_igo").data(data).enter().append("circle").attr("class", "bar_igo")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg*5; })
-                     .attr('r', function(d) { return d.IGO_can*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
+                     .attr('r', function(d) { return d["IGO_"+selection]*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
                      .attr('opacity', 0.8).on("mouseover", function(d) {
 
                             // TOOLTIP : Visibility, transition, opacity, content
@@ -146,7 +149,7 @@ d3.json('./data/thesis_data.json').then((data) => {
     // IAD
       var rounds_iad = svg_plot.selectAll(".bar_iad").data(data).enter().append("circle").attr("class", "bar_iad")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg*6; })
-                     .attr('r', function(d) { return d.IAD_can*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
+                     .attr('r', function(d) { return d["IAD_"+selection]*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
                      .attr('opacity', 0.8).on("mouseover", function(d) {
 
                             // TOOLTIP : Visibility, transition, opacity, content
@@ -163,7 +166,7 @@ d3.json('./data/thesis_data.json').then((data) => {
     // VTI
       var rounds_vti = svg_plot.selectAll(".bar_vti").data(data).enter().append("circle").attr("class", "bar_vti")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg*7; })
-                     .attr('r', function(d) { return d.VTI_can*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
+                     .attr('r', function(d) { return d["VTI_"+selection]*6; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
                      .attr('opacity', 0.8).on("mouseover", function(d) {
 
                             // TOOLTIP : Visibility, transition, opacity, content
@@ -181,7 +184,7 @@ d3.json('./data/thesis_data.json').then((data) => {
     // TRJ
       var rounds_trj = svg_plot.selectAll(".bar_trj").data(data).enter().append("circle").attr("class", "bar_trj")
                      .attr("cx", function(d,i) { return 20+i*width/56; }).attr("cy", function(d) { return marg*8; })
-                     .attr('r', function(d) { return d.TRJ_can*5; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
+                     .attr('r', function(d) { return d["TRJ_"+selection]*5; }).attr("stroke-width", 1.5).attr("stroke", "#808080").attr('stroke-width', 0.75).attr("fill", "#071026")
                      .attr('opacity', 0.8).on("mouseover", function(d) {
 
                             // TOOLTIP : Visibility, transition, opacity, content
@@ -195,55 +198,98 @@ d3.json('./data/thesis_data.json').then((data) => {
                       
     rounds.push(rounds_trj);
     
+    //  }
+     
+    
             
-            
-    //------- Assign values to global variables ------------------    
-    tabs = ['air-india-tab', 'jet-airways-tab', 'spicejet-tab', 'go-air-tab', 'indigo-tab', 'air-asia-tab', 'vistara-tab', 'trujet-tab'];
-    colors = ['#d75b68', '#BF98FF', '#93A7FA', '#6CB9AA', '#FF5388', '#D1A556', '#A5D672', '#92D2F7']
-          
+                
+        //------- Assign values to global variables ------------------    
+        tabs = ['air-india-tab', 'jet-airways-tab', 'spicejet-tab', 'go-air-tab', 'indigo-tab', 'air-asia-tab', 'vistara-tab', 'trujet-tab'];
+        colors = ['#d75b68', '#BF98FF', '#93A7FA', '#6CB9AA', '#FF5388', '#D1A556', '#A5D672', '#92D2F7']
+              
              
 
-    // -------------- Select carrier to update ---------------------
-        for(i=0;i<8;i++){
-            
-             d3.select(`#${tabs[i]}`).on('click', (d,i)=>{
-                id = this.document.activeElement.id;
-                update(d, id);
-            });
-            
-        }
-    
-    
-    // -------------- UPDATE function ---------------------
-     function update(d, id){
-         
-         var index;
-         if(id==tabs[0]) index=0;
-         else if(id==tabs[1]) index=1;
-         else if(id==tabs[2]) index=2;
-         else if(id==tabs[3]) index=3;
-         else if(id==tabs[4]) index=4;
-         else if(id==tabs[5]) index=5;
-         else if(id==tabs[6]) index=6;
-         else if(id==tabs[7]) index=7;
-         
-         for(var i=0;i<8;i++){
-                 if (i==index)
-                 rounds[i].attr('fill',`${colors[i]}`).attr("stroke", 'none');
-                 else
-                 rounds[i].attr('fill','none').attr("stroke", '#808080').attr('stroke-width', 0.75);
+        // -------------- Select carrier to update ---------------------
+            for(i=0;i<8;i++){
+                
+                 d3.select(`#${tabs[i]}`).on('click', (d,i)=>{
+                    id = this.document.activeElement.id;
+                    update(d, id);
+                });
+                
             }
-     }
-
+    
+    
+        // -------------- UPDATE function ---------------------
+         function update(d, id){
+             
+             var index;
+             if(id==tabs[0]) index=0;
+             else if(id==tabs[1]) index=1;
+             else if(id==tabs[2]) index=2;
+             else if(id==tabs[3]) index=3;
+             else if(id==tabs[4]) index=4;
+             else if(id==tabs[5]) index=5;
+             else if(id==tabs[6]) index=6;
+             else if(id==tabs[7]) index=7;
+             
+             for(var i=0;i<8;i++){
+                     if (i==index)
+                     rounds[i].attr('fill',`${colors[i]}`).attr("stroke", 'none');
+                     else
+                     rounds[i].attr('fill','none').attr("stroke", '#808080').attr('stroke-width', 0.75);
+                }
+         }
+}
 
  //tacker
-    var tracker = svg_plot.append('rect').attr('x',0 ).attr('y',0).attr('width',2).attr('height', '100%').attr('fill','#ffffff')
+    var tracker = svg_plot.append('rect').attr('x',0 ).attr('y',0).attr('width',0.5).attr('height', '100%').attr('fill','#efefef')
+    var div_plot = d3.select("#plot").append("div").attr("id", "tooltip").style('visibility','hidden');
 
     svg_plot.on('mousemove', ()=>{
-        console.log(d3.mouse(svg_plot.node())[0])
+        
         var x = d3.mouse(svg_plot.node())[0];
+        
+        var currentMonth = Math.floor(x / (width/56));
+        console.log(data[currentMonth]);
+        
         tracker.attr('x', x);
+        
+         div_plot.transition().duration(40).style("opacity", 1);
+    
+    //   div_plot.text(d3.event.pageX + ", " + d3.event.pageY)
+    //   div_plot.text('AIC: ' + data[currentMonth]['AIC_'+select.value] 
+    //             + '<br>JAI: ' + data[currentMonth]['JAI_'+select.value])
+    
+       div_plot.html('AIC: ' + data[currentMonth]['AIC_'+select.value] 
+                + '<br>JAI: ' + data[currentMonth]['JAI_'+select.value]
+                + '<br>SEG: ' + data[currentMonth]['SEG_'+select.value]
+                + '<br>GOW: ' + data[currentMonth]['GOW_'+select.value]
+                + '<br>IGO: ' + data[currentMonth]['IGO_'+select.value]
+                + '<br>IAD: ' + data[currentMonth]['IAD_'+select.value]
+                + '<br>VTI: ' + data[currentMonth]['VTI_'+select.value]
+                + '<br>TRJ: ' + data[currentMonth]['TRJ_'+select.value]
+                )
+      .style("left", (d3.event.pageX - 100) + "px")
+      .style("top", (d3.event.pageY - 100) + "px");
+        
+    }).on('mouseover', ()=>{
+        div_plot.style("display", "inline");
     })
+    .on("mouseout", function(d) {
+        //  div.transition().duration(500).attr('fill','black').style("opacity", 0);
+         document.getElementById("tooltip").style.visibility='hidden'; 
+    });;
+    
+    var select = document.getElementById("select");
+    
+    select.addEventListener("change", ()=>{
+        console.log(select.value);
+        draw(select.value);
+    });
+    
+    draw('can');
+    
     
 });  
 
